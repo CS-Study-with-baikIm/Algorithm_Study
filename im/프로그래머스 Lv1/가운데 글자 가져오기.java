c3645168e8a8b19f0ed28d059ef212b8a3083ae2
i-am-jiwon <dlark1128@gmail.com>
2024-04-16 14:02:24 +0900

    [Algorithm] 가운데 글자 가져오기

---
+++ im/프로그래머스 Lv1/가운데 글자 가져오기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String s) {
+        /*
+            가운데 글자 가져오기
+            홀수와 짝수 경우를 나누어 문제풀이
+        */
+
+        return s.length() %2 == 1
+                ? "" +s.charAt(s.length()/2)
+                : "" + s.charAt(s.length()/2 -1) +s.charAt(s.length()/2);
+
+        /*
+           substring을 활용하여
+           (s.length()-1)/2 초기조건에서 홀수와 짝수 모두 작동하게 만든 코드
+        */
+        // return s.substring((s.length()-1)/2, s.length()/2 +1);
+
+
+    }
+}
