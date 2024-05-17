da405cb76be18e7db976ca4fa36ab7d85725576b
jonghyunBaik <pigmammam4@gmail.com>
2024-05-18 01:21:57 +0900

    [JAVA] swea18662 등차수열 만들기

---
+++ baik/Algorithm/algorithm_java/src/Solution_18662.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Solution_18662
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            double[] arr = new double[3];
+
+            for(int i = 0; i < 3; i++) {
+                arr[i] = sc.nextDouble();
+            }
+
+            double ans = Math.abs((arr[2] + arr[0]) / 2.0 - arr[1]);
+
+            System.out.printf("#%d %.1f\n", test_case, ans);
+
+        }
+    }
+}
